ac69a29605b4cf54a787210d1f68d0e544b47322
pradyutm <pradyutmandal14@gmail.com>
2021-05-02 17:49:52 +0800

    stored random motivational words

---
+++ script.js
@@ -0,0 +1,9 @@
+let generateRandomNumber = num => {
+    Math.floor(Math.random()*num);
+}
+
+let MotivationalWords = {
+    youshouldalways: ['love yourself.', 'love what you are doing.', 'fight for your dreams.', 'push forward.', 'dream big.','staying true to yourself.'],
+    thereare: ['bright days ahead.', 'more opportunities coming your way.', 'good people about to enter your life.'],
+    keepon: ['pushing yoursef.',"living every day to it's fullest.", 'helping others.', 'making your dreams a reality.']
+};
