e12eca72b9120d138e79e95623990d1bf6b0bf56
pradyutm <pradyutmandal14@gmail.com>
2021-05-02 18:43:00 +0800

    fully functional

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 let generateRandomNumber = num => {
-    Math.floor(Math.random()*num);
+   return Math.floor(Math.random()*num);
 }
+
 
 let MotivationalWords = {
     youshouldalways: ['love yourself.', 'love what you are doing.', 'fight for your dreams.', 'push forward.', 'dream big.','staying true to yourself.'],
     thereare: ['bright days ahead.', 'more opportunities coming your way.', 'good people about to enter your life.'],
     keepon: ['pushing yoursef.',"living every day to it's fullest.", 'helping others.', 'making your dreams a reality.']
 };
+
+let finalStatements = [];
+
+for (let word in MotivationalWords) {
+    let randomIndex = generateRandomNumber(MotivationalWords[word].length);
+
+    switch (word) {
+        case 'youshouldalways':
+            finalStatements.push(`- You should always ${MotivationalWords[word][randomIndex]}`);
+            break;
+        case 'thereare':
+        finalStatements.push(`- There are ${MotivationalWords[word][randomIndex]}`);
+        break; 
+        case 'keepon':
+        finalStatements.push(`- Keep on ${MotivationalWords[word][randomIndex]}`);
+        break; 
+    }
+}
+
+let formatStatements = arr => {
+    const statements = arr.join('\n');
+    console.log(statements)
+}
+
+formatStatements(finalStatements);
